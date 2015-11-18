e65d535b7be22a503da7694a88870d6d872dbf50
TestOnly2 <nagy.tibor@mirango.hu>
2015-11-18 11:12:56 +0100

    initial

---
+++ Project1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expected = 6;
+            var actual = Class1.Add(1, 5);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

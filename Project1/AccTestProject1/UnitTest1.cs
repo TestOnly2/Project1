64b19921ee77b42c929901292b391f37c89afc13
TestOnly2 <nagy.tibor@mirango.hu>
2015-11-18 15:35:12 +0100

    acctest add

---
+++ Project1/AccTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project1;
+
+namespace AccTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var add = Class1.Add(1, 4);
+            var actual = Class1.Sub(add, 6);
+            var expected = -1;
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}

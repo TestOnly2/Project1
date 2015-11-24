ac79c859dd2021eb1aa7f4e79f47757da726f571
TestOnly2 <nagy.tibor@mirango.hu>
2015-11-24 12:49:10 +0100

    Add advanced test

--- Project1/AccTestProject1/UnitTest1.cs
+++ Project1/AccTestProject1/UnitTest1.cs
@@ -7,13 +7,13 @@ namespace AccTestProject1
     [TestClass]
     public class UnitTest1
     {
-        //[TestMethod]
-        //public void TestMethod1()
-        //{
-        //    var add = Class1.Add(1, 4);
-        //    var actual = Class1.Sub(add, 6);
-        //    var expected = -1;
-        //    Assert.AreEqual(expected,actual);
-        //}
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var add = Class1.Add(1, 4);
+            var actual = Class1.Sub(add, 6);
+            var expected = -1;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }

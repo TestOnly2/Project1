d6a9e429a65d03c9dfb0be18f668688a48510c2e
TestOnly2 <nagy.tibor@mirango.hu>
2015-11-24 12:38:22 +0100

    Test Add

--- Project1/UnitTestProject1/UnitTest1.cs
+++ Project1/UnitTestProject1/UnitTest1.cs
@@ -7,13 +7,13 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-        //[TestMethod]
-        //public void TestAdd()
-        //{
-        //    var expected = 6;
-        //    var actual = Class1.Add(1, 5);
-        //    Assert.AreEqual(expected, actual);
-        //}
+        [TestMethod]
+        public void TestAdd()
+        {
+            var expected = 6;
+            var actual = Class1.Add(1, 5);
+            Assert.AreEqual(expected, actual);
+        }
 
         //[TestMethod]
         //public void TestSub()

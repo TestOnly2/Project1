27cb9e70b06a3602318a3aaeb1bcce85bb00ce58
TestOnly2 <nagy.tibor@mirango.hu>
2015-11-24 12:36:07 +0100

    comment out stuff

--- Project1/Project1/Class1.cs
+++ Project1/Project1/Class1.cs
@@ -8,24 +8,24 @@ namespace Project1
 {
     public static class Class1
     {
-        public static int Add(int a, int b)
-        {
-            return a + b;
-        }
+        //public static int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
 
-        public static int Sub(int a, int b)
-        {
-            return a - b;
-        }
+        //public static int Sub(int a, int b)
+        //{
+        //    return a - b;
+        //}
 
-        public static int Mul(int a, int b)
-        {
-            return a * b;
-        }
+        //public static int Mul(int a, int b)
+        //{
+        //    return a * b;
+        //}
 
-        public static int Div(int a, int b)
-        {
-            return a / b;
-        }
+        //public static int Div(int a, int b)
+        //{
+        //    return a / b;
+        //}
     }
 }
--- Project1/UnitTestProject1/UnitTest1.cs
+++ Project1/UnitTestProject1/UnitTest1.cs
@@ -7,36 +7,36 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestAdd()
-        {
-            var expected = 6;
-            var actual = Class1.Add(1, 5);
-            Assert.AreEqual(expected, actual);
-        }
+        //[TestMethod]
+        //public void TestAdd()
+        //{
+        //    var expected = 6;
+        //    var actual = Class1.Add(1, 5);
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod]
-        public void TestSub()
-        {
-            var expected = -4;
-            var actual = Class1.Sub(1, 5);
-            Assert.AreEqual(expected, actual);
-        }
+        //[TestMethod]
+        //public void TestSub()
+        //{
+        //    var expected = -4;
+        //    var actual = Class1.Sub(1, 5);
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod]
-        public void TestMul()
-        {
-            var expected = 20;
-            var actual = Class1.Mul(4, 5);
-            Assert.AreEqual(expected, actual);
-        }
+        //[TestMethod]
+        //public void TestMul()
+        //{
+        //    var expected = 20;
+        //    var actual = Class1.Mul(4, 5);
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod]
-        public void TestDiv()
-        {
-            var expected = 4;
-            var actual = Class1.Div(20, 5);
-            Assert.AreEqual(expected, actual);
-        }
+        //[TestMethod]
+        //public void TestDiv()
+        //{
+        //    var expected = 4;
+        //    var actual = Class1.Div(20, 5);
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }

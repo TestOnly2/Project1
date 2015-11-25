7454cb3356c0e7cf3cba5520d59e781a92f51f50
TestOnly2 <nagy.tibor@mirango.hu>
2015-11-25 13:23:32 +0100

    Add new testproject

---
+++ Project1/UnitTestProject1_2/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project1;
+
+namespace UnitTestProject1_2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+    }
+}
